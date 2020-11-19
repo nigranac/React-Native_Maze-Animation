7bc2b955c5dd8c6211be93d3359286da36db9af9
nigranac <can.argin.16@gmail.com>
2020-11-19 23:46:10 +0300

    poseCreated

--- src/Sample.js
+++ src/Sample.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Dimensions, Text, ScrollView} from 'react-native';
 import posed from 'react-native-pose';
-
+const GridItem = posed.View({
+  RIGHT: {rotate: '45deg'},
+  LEFT: {rotate: '-45deg'},
+});
 
 
 const Sample = () => {
